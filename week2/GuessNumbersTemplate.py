2132d8425fedc805d61e9c7b104860349859febc
Jay Shen <44421655+jshe2304@users.noreply.github.com>
2021-09-30 20:13:24 -0700

    Create GuessNumbersTemplate.py

---
+++ week2/GuessNumbersTemplate.py
@@ -0,0 +1,21 @@
+#A boolean, what for?
+guessed_right = False
+
+#The number you are going to guess
+target_number = 59
+
+
+
+#A while loop
+while not guessed_right:
+
+  #Gets a number from the player
+  inputted_number = int (input("Guess a number\n"))
+  
+
+  #Write your code below
+
+  #Write your code above
+ 
+
+print ('Game Over!')
