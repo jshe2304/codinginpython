f36de7cc0827e0ab60e596bbfd5d53bc42492429
Jay Shen <44421655+jshe2304@users.noreply.github.com>
2021-09-30 20:13:46 -0700

    Create weather.py

---
+++ week2/weather.py
@@ -0,0 +1,8 @@
+is_it_going_to_rain = "True"
+
+if is_it_going_to_rain == True:
+  print ('its going to rain, bring an umbrella')
+elif is_it_going_to_rain == False:
+  print ('its not going to rain. All clear')
+else:
+  print ('im not sure what that means')
